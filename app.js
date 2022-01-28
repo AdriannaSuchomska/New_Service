2cb78703e7d5324464911cc1b6f4cb18252aeba8
Adrianna Suchomska <adrianna.suchomska@gmail.com>
2022-01-28 19:19:27 +0100

    dodanie menu

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');    
-var usersRouter = require('./routes/users');    
+var topNewsRouter = require('./routes/topNews');    
+var questionnaireRouter = require('./routes/questionnaire');    
+var adminRouter = require('./routes/admin');    
+ 
 
 var app = express();                            
 
@@ -19,8 +22,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.locals.path = req.path;
+
+  next();
+});
+
+
 app.use('/', indexRouter);                  
-app.use('/users', usersRouter);
+app.use('/topNews', topNewsRouter);                  
+app.use('/questionnaire', questionnaireRouter);                  
+app.use('/admin', adminRouter);                  
+
 
 // catch 404 and forward to error handler 
 app.use(function(req, res, next) {
