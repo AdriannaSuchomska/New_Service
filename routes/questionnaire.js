7d72c84f6f3f6e68d2e671f652b239a283470cc6
Adrianna Suchomska <adrianna.suchomska@gmail.com>
2022-01-29 13:55:10 +0100

    Ankieta

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Questionnaire = require('../models/questionnaire');
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('questionnaire', { title: 'Ankieta' });
+  const show = !req.session.vote;
+
+  Questionnaire.find({}, (err, data) => {
+    let summary = 0;
+    data.forEach( (item) => {
+      summary += item.vote;
+    });
+
+
+    res.render('questionnaire', { title: 'Ankieta', data, summary });
+  });
+});
+
+router.post('/', (req, res) => {
+  const id = req.body.answer;
+
+  Questionnaire.findOne({_id: id}, (err, data) => {
+    data.vote = data.vote +1;
+    data.save(() => {
+      req.session.vote = 1;
+
+      res.redirect('/questionnaire');
+    });
+  });
 });
 
 module.exports = router;
