5691ce29a7a4635a8e858d86a268e7f262b0df7d
Adrianna Suchomska <adrianna.suchomska@gmail.com>
2022-01-29 12:37:03 +0100

    logowanie administratora dodawanie oraz usuwanie artykułów

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var createError = require('http-errors');    
+var cookieSession = require('cookie-session');
 var express = require('express');            
 var path = require('path');                  
 var cookieParser = require('cookie-parser'); 
 var logger = require('morgan');
+var config = require('./config');
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(config.db);
+}
 
 var indexRouter = require('./routes/index');    
 var topNewsRouter = require('./routes/topNews');    
 var questionnaireRouter = require('./routes/questionnaire');    
 var adminRouter = require('./routes/admin');    
- 
+
+
 
 var app = express();                            
 
@@ -21,6 +31,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession 
+}))
+
 
 app.use(function(req, res, next) {
   res.locals.path = req.path;
